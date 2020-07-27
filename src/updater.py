23b20ae1570a9e2e02fecf44beea2e7aae4e6e20
Amin Ghasembeigi <me@aminbeigi.com>
2020-07-27 14:09:38 +1000

    Store all relevant data in lists

--- src/updater.py
+++ src/updater.py
@@ -1,7 +1,7 @@
 import mechanicalsoup
 import json
 
-""" automatic upadater for index.html
+""" Automatic upadater for index.html.
 
 This script will automatically update the title and description
 of index.html.
@@ -11,12 +11,28 @@ USERNAME = 'aminbeigi'
 API_URL = f'https://api.github.com/users/{USERNAME}/repos'
 
 def updater():
+    # Get access to API in JSON 
     browser = mechanicalsoup.Browser()
     response = browser.get(API_URL)
     data = json.loads(response.text) 
     name_list = []
     description_list = []
-    
+
+    i = 0
+    while(True):
+        try:
+            name_list.append(data[i]['name'])
+            i += 1
+        except IndexError:
+            break
+
+    i = 0
+    while (True):
+        try:
+            description_list.append(data[i]['description'])
+            i += 1
+        except IndexError:
+            break
 
 def main():
     updater()
