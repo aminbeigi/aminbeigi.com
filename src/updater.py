0609d582f9f3be8cc16890bd6158ae2e4f2184ba
Amin Ghasembeigi <me@aminbeigi.com>
2020-07-27 19:01:35 +1000

    Replaces about and title section

--- src/updater.py
+++ src/updater.py
@@ -1,4 +1,5 @@
 import mechanicalsoup
+from bs4 import BeautifulSoup
 import json
 
 """ Automatic upadater for index.html.
@@ -10,32 +11,33 @@ of index.html.
 USERNAME = 'aminbeigi'
 API_URL = f'https://api.github.com/users/{USERNAME}/repos'
 
-def updater():
-    # Get access to API in JSON 
+def update():
+    # connect to API get response in JSON 
     browser = mechanicalsoup.Browser()
     response = browser.get(API_URL)
     data = json.loads(response.text) 
-    name_list = []
-    description_list = []
 
-    i = 0
-    while(True):
-        try:
-            name_list.append(data[i]['name'])
-            i += 1
-        except IndexError:
-            break
+    # open index.html for reading
+    with open('index.html', 'r') as html_file:
+        soup = BeautifulSoup(html_file.read(), features = 'html.parser')
+        
+    # replace each 'section-title'
+    for i, tag in enumerate(soup.find_all('div', {'class': 'section-title'})):
+        tag.string.replace_with(data[i]['name'])
 
-    i = 0
-    while (True):
-        try:
-            description_list.append(data[i]['description'])
-            i += 1
-        except IndexError:
-            break
+    # replace each 'about_seciton'
+    for i, tag in enumerate(soup.find_all('div', {'class': 'about-section'})):
+        tag.string.replace_with(data[i]['description'])
+
+    # store prettified version of modified html
+    new_text = soup.prettify()
+
+    # save file
+    with open('index.html', mode = 'w') as new_html_file:
+        new_html_file.write(new_text)
 
 def main():
-    updater()
+    update()
 
 if __name__ == '__main__':
     main()
