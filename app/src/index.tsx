23c237debd04a2ebdafc7bebdade533fb7690376
Amin Ghasembeigi <me@aminbeigi.com>
2021-02-14 00:39:14 +1100

    Complete TODOs

--- app/src/index.tsx
+++ app/src/index.tsx
@@ -6,8 +6,6 @@ import './index.css';
 import { BrowserRouter as Router } from "react-router-dom";
 
 ReactDOM.render(
-    // TODO: whats strictmode
-    // TODO: whats router
   <React.StrictMode>
       <Router>
             <App />
