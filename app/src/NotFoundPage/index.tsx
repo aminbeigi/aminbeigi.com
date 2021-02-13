cceb2639fb213b2294bbf19f8b49d5f2f2b61fe5
Amin Ghasembeigi <me@aminbeigi.com>
2021-02-13 13:09:56 +1100

    Add styling to error page

--- app/src/NotFoundPage/index.tsx
+++ app/src/NotFoundPage/index.tsx
@@ -2,16 +2,16 @@ import React from 'react';
 import { Title, Tagline, Footer, FooterWrapper } from '../shared/styled';
 import confusedTravolta from './media/confused_travolta.gif';
 
-import { StyledP, StyledButton } from './styled';
+import { StyledImg, StyledP, StyledButton } from './styled';
 
 export const NotFoundPage = () => { 
     return (
         <>
             <Title>Amin Beigi</Title> 
             <Tagline>Computer Scientist • Minimalist</Tagline>
-            <img src={confusedTravolta}/>
+            <StyledImg src={confusedTravolta}/>
             <StyledP>Whoops, the page you are looking for can't be found!</StyledP>
-            <StyledButton>Home Page</StyledButton>
+            <StyledButton href='/'>Home Page</StyledButton>
             <FooterWrapper>
                 <Footer>Copyright © 2021 Amin Ghasembeigi</Footer>
             </FooterWrapper>
--- app/src/NotFoundPage/styled.js
+++ app/src/NotFoundPage/styled.js
@@ -1,10 +1,18 @@
 import styled from 'styled-components';
 import { Button } from 'react-bootstrap';
 
+export const StyledImg = styled.img`
+    margin-top: 10px;
+    margin-bottom: 10px;
+`
 export const StyledP = styled.p`
     font-size: 1rem;
 `
 
 export const StyledButton = styled(Button)`
     font-size: 1rem;
+    background-color: transparent;
+    border: 1px solid #007bff;
+    color: #007bff;
+    cursor: pointer;
 `
