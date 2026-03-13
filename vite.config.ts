5580bbbfa02ff413842f6914042709836180be6d
Amin Beigi <me@aminbeigi.com>
2026-03-14 10:27:09 +1100

    add deply script in package.json also stop splitting js files and only generate one

--- vite.config.ts
+++ vite.config.ts
@@ -5,16 +5,4 @@ import tailwindcss from '@tailwindcss/vite';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          vendor: ['react', 'react-dom'],
-          router: ['react-router-dom'],
-          markdown: ['react-markdown'],
-          highlighting: ['highlight.js'],
-        },
-      },
-    },
-  },
 });
